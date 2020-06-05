e2463f5be78428c5a0bbba8a8241c71094d72237
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-06-04 19:53:17 -0500

    completed the assignment with a bit more heartache than originally planned

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,29 @@
+
+const cards = document.querySelector('.cards');
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+function getUser(usename) {
+  const promise = axios.get(`https://api.github.com/users/${usename}`)
+    promise.then((response) => {
+      console.log(response.data);
+
+      const userData = response.data;
+
+      cards.appendChild(cardMaker({ avatar_url: userData.avatar_url, name: userData.name, login: userData.login, location: userData.location, html_url: userData.html_url, followers: userData.followers, following: userData.following, bio: userData.bio }));
+
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getUser('domikitchen');
+getUser('Aaroneld');
+getUser('Ladrillo');
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -49,6 +70,51 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker(atties) {
+  
+  const { avatar_url, name, login, location, html_url, followers, following, bio } = atties;
+
+  const card = document.createElement('div');
+  const userImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const uname = document.createElement('h3');
+  const username = document.createElement('p');
+  const locat = document.createElement('p');
+  const profile = document.createElement('p');
+  const githubAdress = document.createElement('a');
+  const follow = document.createElement('p');
+  const followed = document.createElement('p');
+  const biog = document.createElement('p');
+
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(uname);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(locat);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(follow);
+  cardInfo.appendChild(followed);
+  cardInfo.appendChild(biog);
+  profile.appendChild(githubAdress);
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  uname.classList.add('name');
+  username.classList.add('username');
+
+  userImg.src = avatar_url;
+  uname.innerHTML = name;
+  username.innerHTML = login;
+  locat.innerHTML = `Location: ${location}`;
+  githubAdress.src = html_url;
+  profile.innerHTML = `Profile: ${githubAdress.innerHTML = html_url}`;
+  follow.innerHTML = followers;
+  followed.innerHTML = following;
+  biog.innerHTML = `Bio: ${bio}`;
+
+
+  return card;
+}
 
 /*
   List of LS Instructors Github username's:
